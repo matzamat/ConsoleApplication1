62c5d468645846f463073616758292da6aa907ae
Максим Потапов <maxim.potapov.2000@gmail.com>
2023-07-26 20:29:48 +0300

    same

--- ConsoleApplication1/8.cpp
+++ ConsoleApplication1/8.cpp
@@ -4,7 +4,9 @@ int main() {
 	setlocale(LC_ALL, "RU");
 
 	int *nums = new int[3];
-
-
+	nums[0] = 45;
+	cout << nums[0] << endl;
+	delete[] nums;
+	cout << "El: " << nums[0] << endl;
 	return 0;
 }
